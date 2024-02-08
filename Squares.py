362af335a81f49d6499e21f155f55289663e37d2
julsy-chen <thejuliachen@gmail.com>
2024-02-07 19:05:45 -0500

    made more efficient

--- Squares.py
+++ Squares.py
@@ -6,12 +6,10 @@ ans = 0
 max = math.sqrt(num)
 
 if max ** 2 == num:
-    print(max)
+    print(int(max))
 else:
     max = int(max)
-    for temp in range(1, num):
+    for temp in range(1, max+1):
         if temp ** 2 < num:
             ans = temp
-        else:
-            print(f"The largest square has side length {ans}.")
-            break
+    print(f"The largest square has side length {ans}.")
